b315c91e352f642ff3a52418d0265869905c4890
Keith Ward <trueinviso@gmail.com>
2017-05-20 13:26:16 -0700

    add pundit policies directory

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :require_login
+
+  layout 'default'
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+    def require_login
+      unless logged_in? # && current_user.active_subscription?
+        redirect_to login_path
+      end
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(request.referrer || root_path)
+    end
 end
