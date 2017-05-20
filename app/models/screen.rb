fe4562e66f563a27c9cfa114e73d787667b20062
Keith Ward <trueinviso@gmail.com>
2017-05-20 10:59:48 -0700

    rename app

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -1,7 +1,7 @@
 class Screen < ApplicationRecord
-  #has_many :favorite_screens
-  #has_many :favorited_by, through: :favorite_screens, source: :user
-  #has_many :downloaded_screens
-  #has_many :downloaded_by, through: :downloaded_screens, source: :user
-  #acts_as_taggable
+  has_many :favorite_screens
+  has_many :favorited_by, through: :favorite_screens, source: :user
+  has_many :downloaded_screens
+  has_many :downloaded_by, through: :downloaded_screens, source: :user
+  acts_as_taggable
 end
